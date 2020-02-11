8a55f43ee571329240f2218ee8ed0b5dc74b8ae5
Sarah Allen <sarah@ultrasaurus.com>
2020-02-10 19:31:38 -0800

    borrowed value does not live long enough

--- mylib/src/tcp.rs
+++ mylib/src/tcp.rs
@@ -1,4 +1,6 @@
+use std::sync::Arc;
 use std::ffi::c_void;
+use std::ptr::NonNull;
 use tokio::runtime::Runtime;
 use tokio::net::TcpStream;
 use std::time::Duration;
@@ -7,33 +9,37 @@ pub struct TcpConnection {
   runtime: Runtime,
   conn: Option<TcpStream>,
   callback: TcpStatusCallback,
-  user_data: UserData,
+  cbdata: CallbackData,
 }
 
- pub struct UserData {
-   data: * const c_void,
- }
- // this might be a bad idea, but I don't know any other option
- // except to trust the caller to put locks around any mutation to this data
- unsafe impl Send for UserData {}
-
-
-pub type TcpStatusCallback = extern "C" fn(code: i32, user_data: UserData) -> * mut c_void;
-
-impl TcpConnection {
-  pub fn trigger_callback(&mut self, code: i32) {
-    let data = UserData { data: std::ptr::null() };
-    (self.callback)(code, data);
-    // want to pass self.user_data, but need to lock it
-  }
+pub struct CallbackData {
+  data: * const c_void,
 }
+// this might be a bad idea, but I don't know any other option
+// except to trust the caller to put locks around any mutation to this data
+unsafe impl Send for CallbackData {}
+
+
+pub type TcpStatusCallback = extern "C" fn(code: i32, user_data: * const c_void) -> * mut c_void;
+
+// impl TcpConnection {
+//   pub fn trigger_callback(&mut self, code: i32) {
+//     let data = CallbackData { data: std::ptr::null() };
+//     (self.callback)(code, data);
+//     // want to pass self.user_data, but need to lock it
+//   }
+// }
 
 #[no_mangle]
-pub extern "C" fn create_tcp(callback: TcpStatusCallback, user_data: UserData) -> * mut TcpConnection {
+pub extern "C" fn create_tcp(callback: TcpStatusCallback, user_data: * const c_void) -> * mut TcpConnection 
+  where TcpStatusCallback : 'static
+{
   assert!(!(callback as *mut c_void).is_null());
   let runtime = Runtime::new().unwrap();
 
-  Box::into_raw(Box::new(TcpConnection { runtime, conn: None, callback, user_data }))
+  Box::into_raw(Box::new(TcpConnection { runtime, conn: None, 
+                                          callback, 
+                                          cbdata: CallbackData { data: user_data } }))
 }
 
 #[no_mangle]
@@ -53,7 +59,7 @@ pub unsafe extern "C" fn tcp_connect_blocking(tcp_ptr: *mut TcpConnection) -> ()
       Err(_) => 404,
     };
   }); 
-  tcp.trigger_callback(code);
+  //tcp.trigger_callback(code);
 }
 
 
@@ -66,13 +72,14 @@ pub unsafe extern "C" fn tcp_connect_async(tcp_ptr: *mut TcpConnection) -> () {
 
   let conn = TcpStream::connect("127.0.0.1:80");
 
+  let callback = &tcp.callback;
   let handle = tcp.runtime.spawn(async {
     let result = conn.await;
     let code = match result {
       Ok(_) => 200,
       Err(_) => 404,
     };
-    tcp.trigger_callback(code);
+    callback(code, std::ptr::null() );
   });
 
 }
